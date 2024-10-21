eb10d09d40e4765e193a29cb57d5169148a8ad65
ArthurSantDev <DevArthurSant@gmail.com>
2024-10-21 15:35:33 -0300

    CORR /section/Projects <> ADD Project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,6 +1,26 @@
 export const PROJECTS = [
     {
         id: 1,
+        title: "Data Analysis - Cost",
+        image: "/assets/projects/da-agricula.svg",
+        description: "Este projeto Python realiza a análise e visualização dos dados de preços de matérias-primas agrícolas usando um banco de dados do Kaggle. Com formato compatível para Júpiter-Notebook e Colab.",
+        stack: [
+            "jupyter", "python", "pandas", "numpy", "seaborn", "matplotlib", "colab"
+        ],
+        markfront: "",
+        markback: "/assets/projects/mark_back.svg",
+        classMark: "project-mark",
+        none: "",
+        class: "project",
+        classReverse: "project reverse",
+        classButton: "button-project deploy-btn",
+        classButtonCant: "button-project deploy-btn cant-btn",
+        github: "https://github.com/ArthurSantDev/DataAnalysis-Agricultural_Raw_Material",
+        deploy: "",
+
+    },
+    {
+        id: 2,
         title: "HighTrip",
         image: "/assets/projects/hightrip.svg",
         description: "Landing Page para um App de viagens e aventura. Com UX/UI Design responsivo e atrativo, utilizando o método 'Mobile-First' do Framework Tailwind para foco maior na responsividade. Next.Js e TypeScript para componentização e uso de Props.",
@@ -20,7 +40,7 @@ export const PROJECTS = [
 
     },
     {
-        id: 2,
+        id: 3,
         title: "To Do List",
         image: "/assets/projects/todolist.svg",
         description: "Projeto de uma To-Do list, utilizando o framework 'Vite' para React como base do desenvolvimento, trazendo layout responsivo e intuitivo. Podendo pesquisar tasks feitas e não feitas, filtrar por tarefas como 'Pessoais', 'Trabalho', 'Contas' e 'Estudos'.",
@@ -77,7 +97,7 @@ export const PROJECTS = [
         deploy: "",
     },
     {
-        id: 5,
+        id: 6,
         title: "Malta University",
         image: "/assets/projects/malta.svg",
         description: "Este projeto consiste em uma Landing Page para a área de cursos industriais de uma Universidade. Todo o UI Design foi feito por 'Maham Bukhari' em seu perfil no Behance.",
@@ -96,7 +116,7 @@ export const PROJECTS = [
         deploy: "https://university-of-malta.vercel.app/",
     },
     {
-        id: 6,
+        id: 7,
         title: "Nike Store",
         image: "/assets/projects/nike.svg",
         description: "E-commerce com estética e produtos da Nike que possui sua base em Vite JS, React Js e Tailwind. Responsivo para dispositivos, com tema Dark e 'carrinho' que é atualizado de acordo com quantidade e tamanho.",
@@ -115,7 +135,7 @@ export const PROJECTS = [
         deploy: "https://nike-air.vercel.app/",
     },
     {
-        id: 7,
+        id: 8,
         title: "Beats",
         image: "/assets/projects/beats.svg",
         description: "Landing Page para marca Beats, possuindo animações dinâmicas por meio da tecnologia ScrollReveal.Js, UX/UI Design moderno e responsivo para diversos tamanhos de Smartphones.",
@@ -134,7 +154,7 @@ export const PROJECTS = [
         deploy: "https://beats-arthur.vercel.app/",
     },
     {
-        id: 8,
+        id: 9,
         title: "API RestFull",
         image: "/assets/projects/fastapi.svg",
         description: "API RestFull utilizando Python como base, possuindo as funções GET, POST, PUT, UPDATE e DELETE. Com implementação de pesquisa por categorias como nos exemplos do arquivo Test_Server.py no repositório.",
